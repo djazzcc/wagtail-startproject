d0f14483608940ff64d13233ab3206a415e28613
Azat <8280770+azataiot@users.noreply.github.com>
2024-07-29 10:24:07 +0300

    wagtail project 6.1.3 done

---
+++ 6.1.3/djazz/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-4ogyno)z%msv)0flb=)#0_&fub2oxay9j8si@7io3o#07#n1+9"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
