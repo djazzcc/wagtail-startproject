917c8bb25824cc035114584d4d2de1046aa53a22
Azat <8280770+azataiot@users.noreply.github.com>
2024-08-14 13:35:38 +0300

    6.2 added

---
+++ 6.2/djazz/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-j0792q5#%j_7^6@+=izrq&-fvs&o91^r_xg+*!23qr&t&puk-o"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
